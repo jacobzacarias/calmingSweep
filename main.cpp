0f77fd9ee5cae8b715bbecd35d91bb868eeb6604
Jacob Zacarias <101657772+jacobzacarias@users.noreply.github.com>
2025-04-14 07:40:28 -0500

    Create main.cpp
    Initial commit for main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "reader.h"
+#include "graph.h"
+#include <sstream>
+#include <string>
+#include <cstring>
+#include "search.h"
+#include <chrono>
+#include "webserver.h"
+
+int main (int argc, char* argv[]){
+  Webserver::run_server(argv[1], argv[2]);
+}
