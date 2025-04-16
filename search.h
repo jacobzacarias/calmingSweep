33242f5a7f5b24538389e55a2dd4cd5c9bb62883
Jacob Zacarias <101657772+jacobzacarias@users.noreply.github.com>
2025-04-16 07:17:01 -0500

    Create search.h
    Including header file for search.cpp

---
+++ search.h
@@ -0,0 +1,40 @@
+
+#ifndef SEARCH_H
+#define SEARCH_H
+#include "graph.h"
+#include "result.h"
+#include <vector>
+#include <iostream>
+#include <queue>
+#include <cstdint>
+#include <utility>
+#include <map>
+using namespace std;
+
+struct sort_operator
+{
+  inline bool operator() (const pair<int, int>& t1, const pair<int, int> & t2)
+  {
+    return (get<0>(t1) > get<0>(t2));
+  }
+};
+class Search
+{
+ public:
+  Graph* g;
+  Search();
+  std::vector<bool> visited;
+  std::vector<int> parents;
+  std::vector<int> distances;
+  std::vector<int> tapVisited;
+  std::vector<int> tapParent;
+  Search(Graph* g);
+  std::priority_queue<pair<int, int>, std::vector<pair<int, int>>, sort_operator> pq;
+  void oneToOne(int source, int target, Result* result);
+  void oneToMany(int source, std::vector<int>* targets , map<int, Result>* resultMap);
+  void getPath(int source,int currNode, vector<Node>* path);
+  void expand(int source, int costs);
+  void reset();
+};
+
+#endif
